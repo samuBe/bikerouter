b54b22d729c100bdcfb62c6d4f241199451e7d2f
Samuel Berton <samuel.berton@student.kuleuven.be>
2024-05-18 15:59:44 +0200

    Added llm connection

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import replicate
 
 st.write('hello world')
+
+if 'REPLICATE_API_TOKEN' in st.secrets:
+    replicate_api = st.secrets['REPLICATE_API_TOKEN']
+
+
+input = {
+    "prompt": "List 10 places to visit in 2024",
+    "temperature": 0.2
+}
+
+output = replicate.run(
+    "snowflake/snowflake-arctic-instruct",
+    input=input
+)
+st.write("".join(output))
