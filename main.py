a7f32e77446d11ec9baf83d5eaccd3c0c993e0e9
Samuel Berton <samuel.berton@student.kuleuven.be>
2024-05-19 14:57:57 +0200

    Finished the llm connection with langchain

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import streamlit as st
+from langchain.chains import LLMChain
 import replicate
+from langchain_community.llms import Replicate
+from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import CommaSeparatedListOutputParser
 import os
 
+# Put some nice things at the top
+st.title('Bike router')
 st.write('hello world')
 
-input = {
-    "prompt": "List 10 places to visit in 2024",
-    "temperature": 0.2
-}
+city = st.text_input("Enter the city you want to visit", key="city")
 
-output = replicate.run(
-    "snowflake/snowflake-arctic-instruct",
-    input=input
-)
-st.write("".join(output))
+
+@st.cache_resource
+def getChain():
+
+    output_parser = CommaSeparatedListOutputParser()
+    format_instructions = output_parser.get_format_instructions()
+
+    llm = Replicate(
+               model="snowflake/snowflake-arctic-instruct",
+               model_kwargs={'temperature':0}
+    )
+
+
+    prompt = PromptTemplate(
+        template="""Return a comma-separated list of the 10 best landmarks in {city}. Only return the list
+        {format_instructions}
+            """,
+        input_variables=["city"],
+        partial_variables={"format_instructions": format_instructions},
+    )
+
+    chain = prompt | llm | output_parser
+    return chain
+
+
+# Run the llm
+chain = getChain()
+if city and len(city)>0:
+    output = chain.invoke({"city": city})
+    print(output)
+    st.write(output)
+
