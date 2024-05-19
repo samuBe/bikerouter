501decb41c6a63dcf3527955f081f286f8d6954d
Samuel Berton <samuel.berton@student.kuleuven.be>
2024-05-19 15:58:31 +0200

    Added the search bar

--- main.py
+++ main.py
@@ -5,13 +5,43 @@ from langchain_community.llms import Replicate
 from langchain_core.prompts import PromptTemplate
 from langchain.output_parsers import CommaSeparatedListOutputParser
 import os
+from streamlit_searchbox import st_searchbox
+from typing import List 
+import requests
+import uuid
+
 
 # Put some nice things at the top
 st.title('Bike router')
 st.write('hello world')
 
-city = st.text_input("Enter the city you want to visit", key="city")
+mapbox_token = os.getenv('MAPBOX_TOKEN')
+token = uuid.uuid4()
 
+# function with list of labels
+def search_city(searchterm: str) -> List[any]:
+    if len(searchterm)<2:
+        return []
+    # sanitize!!!!!
+    search_text = searchterm
+    url = "https://api.mapbox.com/search/searchbox/v1/suggest"
+    params = {"q": searchterm, "access_token": mapbox_token,"session_token": token, "types": "city"}
+    res = requests.get(url, params=params)
+    if res.status_code != 200:
+       return [] 
+    try:
+        suggestions = res.json()['suggestions']
+        results = list(map(lambda s: s['name'] + ', ' + s['place_formatted'],suggestions))
+        return results
+    except:
+        return []
+    return []
+
+# pass search function to searchbox
+city = st_searchbox(
+    search_city,
+    key="city",
+)
 
 @st.cache_resource
 def getChain():
