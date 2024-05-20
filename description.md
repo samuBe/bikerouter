023e02928dc409b8fb0d7c1813174fc133f5a14c
Samuel Berton <samuel.berton@student.kuleuven.be>
2024-05-20 16:51:40 +0200

    Added the description of the bike route

---
+++ description.md
@@ -0,0 +1,7 @@
+## Welcome to BikeRouter!
+
+This project combines the power of LLMs with location data and everyone's favourite computational problem: The Travelling Salesman problem!
+
+* Enter the city you want to visit and you will get 10 must-visits in the city of choice! 
+* Pick the locations you want to visit 
+* Generate the shortest path between these locations
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from typing import List
 import requests
 import uuid
 import pandas as pd
+import markdown
 
 
 # Put some nice things at the top
@@ -114,7 +115,6 @@ def get_landmark_locations(landmarks, long, lat):
 
     # put into a pandas dataframe
     df = pd.DataFrame(data=data, columns=['Name', 'longitude', 'latitude', 'Include'])
-    print(df)
     return df
 
 @st.cache_data
@@ -123,25 +123,50 @@ def run_llm(parameters):
     return chain.invoke(parameters)
 
 @st.cache_data
-def tsp(user_input):
+def tsp(chosen_landmarks):
     profile = "mapbox/cycling"
-    coordinates = ";".join([f"{row['longitude']},{row['latitude']}" for index, row in user_input[user_input['Include']].iterrows()])
+    coordinates = ";".join([f"{row['longitude']},{row['latitude']}" for index, row in chosen_landmarks.iterrows()])
     link = f"https://api.mapbox.com/optimized-trips/v1/{profile}/{coordinates}"
     params = {"access_token": mapbox_token}
     res = requests.get(link, params=params)
-    print(res.text)
     if res.status_code != 200:
         return []
     try:
-        print(res.text)
         return res.json()
     except:
         return []
     return []
 
 @st.cache_data
-def create_route():
+def create_route(parameters):
     llm = get_llm()
+    prompt = PromptTemplate(
+        template="""You are an experienced tour guide in {city}. Create a bike route for {city} in markdown, passing by the following landmarks: {landmarks}. The route should be clearly connected through the text.
+        An excerpt for Paris: 
+        ## Place d'Alma
+        Our first stop is Place d'Alma with a superb view of the **Eiffel Tower**. The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France.
+        It is named after the engineer Gustave Eiffel, whose company designed and built the tower from 1887 to 1889. 
+        Locally nicknamed "La dame de fer" (French for "Iron Lady"), it was constructed as the centerpiece of the 1889 World's Fair, and to crown the centennial anniversary of the French Revolution. 
+        Although initially criticised by some of France's leading artists and intellectuals for its design, it has since become a global cultural icon of France and one of the most recognisable structures in the world.
+        Next step, the **Arc the Triomphe**...
+        """, 
+       input_variables=["landmarks", "city"]
+    )
+    chain = prompt | llm
+    return chain.invoke(parameters)
+
+@st.cache_data
+def to_html(data, filename='BikeRouter-route'):
+    return markdown.markdown(data)
+
+if 'route' not in st.session_state:
+    st.session_state.route = None
+
+def generate_route(city, stops):
+    parameters = {"city": city['properties']['full_address'], "landmarks": stops}
+    route = create_route(parameters)
+    st.session_state.route = route
+    pass
 
 
 # Run the llm
@@ -155,21 +180,30 @@ if city_id and len(city_id)>0:
     landmark_locations = st.session_state.landmark_locations
     user_input = st.data_editor(landmark_locations, hide_index=True, disabled=('name', 'longitude', 'latitude'), 
                    column_config= {'longitude': None, 'latitude': None}, key='user_input', use_container_width=True)
-    st.map(user_input[user_input['Include']])
-    output = tsp(user_input)
-    st.json(output)
+    chosen_landmarks = user_input[user_input['Include']]
+    st.map(chosen_landmarks)
+    output = tsp(chosen_landmarks)
 
-    # Download the route description
-    route = create_route()
+    distance = output['trips'][0]['distance']
+    st.write(f"Total distance: {distance}m")
 
-    #
-    route_word = None
+    waypoints = map(lambda wp: wp['waypoint_index'],output['waypoints'])
+    stops  = chosen_landmarks.iloc[waypoints, :]
+    stops_string = "\n".join([f"{index}: {row['Name']}" for index, row in stops.iterrows()])
+
+    st.button('Generate route', on_click=lambda : generate_route(city, stops_string))
+    
+
+route = st.session_state.route
+if route and len(route)>0:
+    st.markdown(route)
+
+    route_html = to_html(route)
 
     st.download_button(
             label='Download the bike route!',
-            data=route_word,
-            file_name='',
-            mime=''
+            data=route_html,
+            file_name='BikeRouter-route.html',
+            mime='text/html'
     )
-    
 
